596ea9fb75d8b1f410650d758e6f15fdd1abc6ed
Hannah Smith <hannah.smith16@justice.gov.uk>
2024-08-23 17:09:27 +0100

    change to batched_operations_enabled

--- main.tf
+++ main.tf
@@ -13,11 +13,11 @@ resource "azurerm_servicebus_subscription" "servicebus_subscription" {
   forward_to                        = var.forward_to
   forward_dead_lettered_messages_to = var.forward_dead_lettered_messages_to
 
-  requires_session                     = var.requires_session
-  dead_lettering_on_message_expiration = true
-  enable_batched_operations            = false
-  default_message_ttl                  = "P10675199DT2H48M5.4775807S"
-  auto_delete_on_idle                  = "P10675199DT2H48M5.4775807S"
+  requires_session                      = var.requires_session
+  dead_lettering_on_message_expiration  = true
+  batched_operations_enabled            = false
+  default_message_ttl                   = "P10675199DT2H48M5.4775807S"
+  auto_delete_on_idle                   = "P10675199DT2H48M5.4775807S"
 }
 
 resource "azurerm_servicebus_subscription_rule" "sql_filter_rule" {
