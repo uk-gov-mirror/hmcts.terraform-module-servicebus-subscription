69e60fbc0dd959f006e3c677bbc4fe4862c5a8f2
Hannah Smith <hannah.smith16@justice.gov.uk>
2024-08-28 12:26:13 +0100

    test separately

---
+++ example/main.tf
@@ -0,0 +1,25 @@
+resource "azurerm_resource_group" "this" {
+  name     = "${var.product}-${var.env}-rg"
+  location = var.location
+
+  tags = module.tags.common_tags
+}
+
+module "this" {
+  source  = "../"
+
+  name                = "plum-subscription"
+  namespace_name      = "plum-servicebus-sandbox"
+  topic_name          = "plum-to-cft"
+
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+# projects run on Jenkins do not need this module should and should just pass through var.common_tags instead
+module "tags" {
+  source       = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
+  environment  = var.env
+  product      = var.product
+  builtFrom    = var.builtFrom
+  expiresAfter = "2023-01-30"
+}
---
+++ example/variables.tf
@@ -0,0 +1,15 @@
+variable "env" {
+  default = "sandbox"
+}
+
+variable "product" {
+  default = "platops"
+}
+
+variable "builtFrom" {
+  default = "local"
+}
+
+variable "location" {
+  default = "UK South"
+}
